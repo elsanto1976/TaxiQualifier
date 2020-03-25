0cab93bb363436497a45e9294491ec85de62ec00
elsanto1976 <36921825+elsanto1976@users.noreply.github.com>
2020-03-25 17:22:07 -0500

    Multi Lenguage 1 parte 37

---
+++ Taxi.Prism/Taxi.Prism/Interfaces/ILocalize.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace Taxi.Prism.Interfaces
+{
+	public interface ILocalize
+	{
+		CultureInfo GetCurrentCultureInfo();
+
+		void SetLocale(CultureInfo ci);
+	}
+}
+
