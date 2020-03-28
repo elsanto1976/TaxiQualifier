3b637536d2ed109d1747c466d31eefa0dfb89074
elsanto1976 <36921825+elsanto1976@users.noreply.github.com>
2020-03-28 14:33:22 -0500

    API for Complete trip

---
+++ Taxi.Common/Models/IncidentRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taxi.Common.Models
+{
+    public class IncidentRequest : TripRequest
+    {
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Remarks { get; set; }
+    }
+}
+
