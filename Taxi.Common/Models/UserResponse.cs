2f00576c116d33c61feeb523b80ac29c3502141e
elsanto1976 <36921825+elsanto1976@users.noreply.github.com>
2020-04-11 15:38:30 -0500

    Login App 4

--- Taxi.Common/Models/UserResponse.cs
+++ Taxi.Common/Models/UserResponse.cs
@@ -25,5 +25,9 @@ namespace Taxi.Common.Models
         public string FullName => $"{FirstName} {LastName}";
 
         public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+
+        public string PictureFullPath => string.IsNullOrEmpty(PicturePath)
+            ? "https://taxiwebnala.azurewebsites.net//images/noimage.png"
+            : $"https://taxiwebnala.azurewebsites.net{PicturePath.Substring(1)}";
     }
 }
